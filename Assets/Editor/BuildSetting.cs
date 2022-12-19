66ddae723a16550bf2e800ca36ccfdb5721087b8
bkkim <bkbk0329@naver.com>
2022-12-19 11:23:14 +0900

    Fixing Build

--- Assets/Editor/BuildSetting.cs
+++ Assets/Editor/BuildSetting.cs
@@ -6,43 +6,39 @@ using UnityEditor;
 
 public class BuildSetting : MonoBehaviour
 {
-    // Current Path
-    private static readonly string cPath = Directory.GetCurrentDirectory();
-    // Build File Path
-    private static readonly string path = cPath + "Build";
-
     public static void MyBuild()
     {
+        string cPath = System.IO.Directory.GetCurrentDirectory();
+        string path = cPath + "Build";
+
         // 해당 path의 정보
-        FileInfo pathInfo = new FileInfo(path);
+        System.IO.FileInfo pathInfo = new System.IO.FileInfo(path);
         // Build 할 씬들
         string[] scene = { "Assets/Scenes/SampleScene.unity" };
         // 그 씬들의 path 정보
-        FileInfo[] Scenes = new FileInfo[scene.Length];
+        System.IO.FileInfo[] Scenes = new System.IO.FileInfo[scene.Length];
 
         // 해당 주소에 파일이 존재하는지
         if(pathInfo.Exists == false)
         {
             // 해당 주소에 새로 폴더 생성
-            Directory.CreateDirectory(path);
+            System.IO.Directory.CreateDirectory(path);
         }
 
         // 씬들 주소 정보 담아서 체크
         for(int i= 0; i < Scenes.Length; i++)
         {
-            Scenes[i] = new FileInfo(cPath+ "/" + scene[i]);
+            Scenes[i] = new System.IO.FileInfo(cPath+ "/" + scene[i]);
 
             // 해당 파일이 없으면 
             if(Scenes[i].Exists == false)
             {
-                // 에러 뱉어내고
-                Debug.LogError("Not Found Scene");
                 // 빌드 안함
                 return;
             }
         }
 
         // 실질적인 빌드
-        BuildPipeline.BuildPlayer(scene, path + "build.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+        UnityEditor.BuildPipeline.BuildPlayer(scene, path + "build.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
     }
 }
