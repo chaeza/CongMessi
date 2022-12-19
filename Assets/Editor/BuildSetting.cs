c1197fd6554a90409ce1e3f9de8245f9ceecaa2b
bkkim <bkbk0329@naver.com>
2022-12-19 12:33:00 +0900

    aaa

--- Assets/Editor/BuildSetting.cs
+++ Assets/Editor/BuildSetting.cs
@@ -8,11 +8,12 @@ public class BuildSetting : MonoBehaviour
 {
     public static void MyBuild()
     {
-        string cPath = System.IO.Directory.GetCurrentDirectory();
+        string cPath = Directory.GetCurrentDirectory();
+        Debug.LogError(cPath);
         string path = cPath + "/Build";
 
         // 해당 path의 정보
-        FileInfo pathInfo = new System.IO.FileInfo(path);
+        FileInfo pathInfo = new FileInfo(path);
         // Build 할 씬들
         string[] scene = { "Assets/Scenes/SampleScene.unity" };
         // 그 씬들의 path 정보
