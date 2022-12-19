a03735ec6e3f5f9721901690ad7410ae189d095f
bkkim <bkbk0329@naver.com>
2022-12-19 12:09:18 +0900

    Fixing

--- Assets/Editor/BuildSetting.cs
+++ Assets/Editor/BuildSetting.cs
@@ -6,27 +6,25 @@ using UnityEditor;
 
 public class BuildSetting : MonoBehaviour
 {
-    [UnityEditor.MenuItem("MyMenu/MyBuild", false, 1)]
     public static void MyBuild()
     {
         string cPath = System.IO.Directory.GetCurrentDirectory();
-        string path = cPath + "/Build/";
+        string path = cPath + "/Build";
 
         // 해당 path의 정보
-        System.IO.FileInfo pathInfo = new System.IO.FileInfo("C:/ProgramData/Jenkins.jenkins/workspace/congMessi_Build/Build");
+        FileInfo pathInfo = new System.IO.FileInfo(path);
         // Build 할 씬들
         string[] scene = { "Assets/Scenes/SampleScene.unity" };
         // 그 씬들의 path 정보
-        System.IO.FileInfo[] Scenes = new System.IO.FileInfo[scene.Length];
 
         // 해당 주소에 파일이 존재하는지
         if(pathInfo.Exists == false)
         {
             // 해당 주소에 새로 폴더 생성
-            System.IO.Directory.CreateDirectory("C:/ProgramData/Jenkins.jenkins/workspace/congMessi_Build/Build");
+            Directory.CreateDirectory(path);
         }
-
         // 실질적인 빌드
-        UnityEditor.BuildPipeline.BuildPlayer(scene, "C:/ProgramData/Jenkins.jenkins/workspace/congMessi_Build/Build/build.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scene, path + "/build.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
     }
+
 }
